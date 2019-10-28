ebec06e6c329efe23699822b315165c72d1fb180
MkLHX <mickael.lehoux@gmail.com>
2019-10-28 12:52:39 +0100

    updt readme  & examples

--- example/PH_Calibration.py
+++ example/PH_Calibration.py
@@ -1,31 +1,32 @@
 import sys
-sys.path.append('../')
-# sys.path.append('../libs') #includes
-# sys.path.append('../src')  #library
-import time
-ADS1115_REG_CONFIG_PGA_6_144V        = 0x00 # 6.144V range = Gain 2/3
-ADS1115_REG_CONFIG_PGA_4_096V        = 0x02 # 4.096V range = Gain 1
-ADS1115_REG_CONFIG_PGA_2_048V        = 0x04 # 2.048V range = Gain 2 (default)
-ADS1115_REG_CONFIG_PGA_1_024V        = 0x06 # 1.024V range = Gain 4
-ADS1115_REG_CONFIG_PGA_0_512V        = 0x08 # 0.512V range = Gain 8
-ADS1115_REG_CONFIG_PGA_0_256V        = 0x0A # 0.256V range = Gain 16
+sys.path.insert(0,'libs/DFRobot_ADS1115/RaspberryPi/Python/')
+sys.path.insert(0,'libs/GreenPonik_PH_Python/src/')
 
-from libs.DFRobot_ADS1115.RaspberryPi.Python.DFRobot_ADS1115 	import ADS1115
-from src.GreenPonik_PH      									import GreenPonik_PH
+from DFRobot_ADS1115 import ADS1115
+from GreenPonik_PH import GreenPonik_PH
+
+ADS1115_REG_CONFIG_PGA_6_144V = 0x00  # 6.144V range = Gain 2/3
+ADS1115_REG_CONFIG_PGA_4_096V = 0x02  # 4.096V range = Gain 1
+ADS1115_REG_CONFIG_PGA_2_048V = 0x04  # 2.048V range = Gain 2 (default)
+ADS1115_REG_CONFIG_PGA_1_024V = 0x06  # 1.024V range = Gain 4
+ADS1115_REG_CONFIG_PGA_0_512V = 0x08  # 0.512V range = Gain 8
+ADS1115_REG_CONFIG_PGA_0_256V = 0x0A  # 0.256V range = Gain 16
 
 ads1115 = ADS1115()
-ph      = GreenPonik_PH()
-
+ph = GreenPonik_PH()
 ph.begin()
-while True :
-	temperature = 25
-	#Set the IIC address
-	ads1115.setAddr_ADS1115(0x48)
-	#Sets the gain and input voltage range.
-	ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
-	#Get the Digital Value of Analog of selected channel
-	adc0 = ads1115.readVoltage(0)
-	print("A0:%dmV "%(adc0['r']))
-	#Calibrate the calibration data
-	ph.calibration(adc0['r'])
-	time.sleep(1.0)
+
+
+def calibration():
+    global ads1115
+    global ph
+    # Set the IIC address
+    ads1115.setAddr_ADS1115(0x48)
+    # Sets the gain and input voltage range.
+    ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
+    # Get the Digital Value of Analog of selected channel
+    adc1 = ads1115.readVoltage(1)
+    return ph.calibration(adc1['r'])
+
+
+calibration()
--- src/GreenPonik_PH.py
+++ src/GreenPonik_PH.py
@@ -17,11 +17,9 @@ https://github.com/DFRobot/DFRobot_ADS1115/tree/master/RaspberryPi/Python
 import time
 import sys
 
-_temperature = 25.0
-_acidVoltage = 2032.44
-_neutralVoltage = 1500.0
-
-
+# _temperature      = 25.0
+_acidVoltage      = 2032.44
+_neutralVoltage   = 1500.0
 class GreenPonik_PH():
 	def begin(self):
 		global _acidVoltage
@@ -34,11 +32,12 @@ class GreenPonik_PH():
 				acidVoltageLine    = f.readline()
 				acidVoltageLine    = acidVoltageLine.strip('acidVoltage=')
 				_acidVoltage       = float(acidVoltageLine)
-		except :
+		except:
 			self.reset()
 			pass
 
-	def readPH(self,voltage,temperature):
+	# def readPH(self,voltage,temperature):
+	def readPH(self,voltage):
 		global _acidVoltage
 		global _neutralVoltage
 		slope     = (7.0-4.0)/((_neutralVoltage-1500.0)/3.0 - (_acidVoltage-1500.0)/3.0)
@@ -46,6 +45,8 @@ class GreenPonik_PH():
 		_phValue  = slope*(voltage-1500.0)/3.0+intercept
 		round(_phValue,2)
 		return _phValue
+
+
 	def calibration(self,voltage):
 		if (voltage>1322 and voltage<1678):
 			print(">>>Buffer Solution:7.0")
@@ -55,8 +56,11 @@ class GreenPonik_PH():
 			f=open('phdata.txt','w+')
 			f.writelines(flist)
 			f.close()
+			status_msg = ">>>PH:7.0 Calibration completed,Please enter Ctrl+C exit calibration in 5 seconds"
 			print(">>>PH:7.0 Calibration completed,Please enter Ctrl+C exit calibration in 5 seconds")
 			time.sleep(5.0)
+			cal_res = {'status': 7,'status_message': status_msg}
+			return cal_res
 		elif (voltage>1854 and voltage<2210):
 			print(">>>Buffer Solution:4.0")
 			f=open('phdata.txt','r+')
@@ -65,10 +69,18 @@ class GreenPonik_PH():
 			f=open('phdata.txt','w+')
 			f.writelines(flist)
 			f.close()
-			print(">>>PH:4.0 Calibration completed,Please enter Ctrl+C exit calibration in 5 seconds")
+			status_msg = ">>>PH:4.0 Calibration completed,Please enter Ctrl+C exit calibration in 5 seconds"
+			print(status_msg)
 			time.sleep(5.0)
+			cal_res = {'status': 4,'status_message': status_msg}
+			return cal_res
 		else:
-			print(">>>Buffer Solution Error Try Again<<<")
+			status_msg = ">>>Buffer Solution Error Try Again<<<"
+			print(status_msg)
+			cal_res = {'status': 9999,'status_message': status_msg}
+			return cal_res
+
+
 	def reset(self):
 		_acidVoltage    = 2032.44
 		_neutralVoltage = 1500.0
